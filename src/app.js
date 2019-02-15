9fc6ec1446209433acab857394603e7b6820e867
Nate Choi <jereno413@gmail.com>
2019-02-15 13:53:03 +0900

    Set normalize.css and React.Router

--- src/app.js
+++ src/app.js
@@ -1,8 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles/styles.scss'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom'
+import 'normalize.css/normalize.css'
+import './styles/styles.scss';
 
-console.log('hello~')
+const MainPage = () => (
+    <div>
+      This is Main Page :
+    </div>
+  );
+  
+  const ServicePage = () => (
+    <div>
+      This is Service Page.
+    </div>
+  );
 
-const template = <p>Hello World</p>
-ReactDOM.render(template, document.getElementById('app'))
+  const TeamPage= () => (
+      <div>
+        This is Team Page.
+      </div>
+  )
+
+  const ContactPage= () => (
+    <div>
+      This is Contact Page.
+    </div>
+)
+  
+  const routes = (
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={MainPage} exact={true} />
+        <Route path="/service" component={ServicePage} />
+        <Route path="/team" component={TeamPage} />
+        <Route path="/contact" component={ContactPage} />
+      </div>
+    </BrowserRouter>
+  );
+  
+  ReactDOM.render(routes, document.getElementById('app'));
+  
+  
