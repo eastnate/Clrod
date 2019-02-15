1dd67f5525f1b8eb84af5ef55aa98abe80689e0e
Nate Choi <jereno413@gmail.com>
2019-02-15 13:09:44 +0900

    Set SCSS

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/styles.scss'
+
+console.log('hello~')
+
+const template = <p>Hello World</p>
+ReactDOM.render(template, document.getElementById('app'))
