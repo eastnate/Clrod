bca2333d9c69622478c5263c5f5534681ced23aa
Nate Choi <jereno413@gmail.com>
2019-02-26 15:13:52 +0900

    Update Service Page

--- src/ServicePage/ServicePage.js
+++ src/ServicePage/ServicePage.js
@@ -7,30 +7,63 @@ import './ServicePage.scss'
 const ServicePage = () => (
     <div className="service">
 
-      <div className = 'main__div'>
-        <div>Velbi.io</div>
-        <p>벨비는 어쩌구 저쩌구 입니다.</p>
-      </div>
-        
-      <div className = 'main__div'>
-        <div>comicstack</div>
-        <p>코스는 어쩌구 저쩌구 입니다. </p>
-      </div>
+      <img className="service__image" src="resource/service2.png" alt="loading.." />
+      <p className="service__text">Beyond. </p>
+      <p className="service__text__sub">
+      Clrod has always been growing in the global market.
+      </p>
 
-      <div className = 'main__div'>
-        <div>cudi.io</div>
-        <p>쿠디는 어쩌구 저쩌구 입니다. </p>
-      </div>
+      <h3 className="service__title">Service</h3>
+      <div className = "service_contents">
 
-      <div className = 'main__div'>
-        <div>solla.io</div>
-        <p>솔라는 어쩌구 저쩌구 입니다. </p>
-      </div>
+      
+      
 
-      <div className = 'main__div'>
-        <div>Oceangray</div>
-        <p>오션그레이는 어쩌구 저쩌구 입니다. </p>
-      </div>
+<figure class="snip1321"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample26.jpg" alt="sq-sample26"/>
+  <figcaption><i class="ion-upload"></i>
+    <h4>Velbi.io</h4>
+    <h2>벨비</h2>
+  </figcaption><a href="#"></a>
+</figure>
+
+
+
+
+<figure class="snip1321"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample28.jpg" alt="sq-sample28"/>
+  <figcaption><i class="ion-share"></i>
+    <h4>ComicStack</h4>
+    <h2>코스</h2>
+  </figcaption><a href="#"></a>
+</figure>
+
+
+<figure class="snip1321"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample26.jpg" alt="sq-sample26"/>
+  <figcaption><i class="ion-upload"></i>
+    <h4>Cudi    </h4>
+    <h2>쿠디</h2>
+  </figcaption><a href="#"></a>
+</figure>
+
+
+<figure class="snip1321"><img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample26.jpg" alt="sq-sample26"/>
+  <figcaption><i class="ion-upload"></i>
+    <h4>Solla    </h4>
+    <h2>솔라</h2>
+  </figcaption><a href="#"></a>
+</figure>
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
     </div>
   )
 
--- src/TeamPage/TeamPage.js
+++ src/TeamPage/TeamPage.js
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom'
 import { Link } from 'react-router-dom'
 import 'normalize.css/normalize.css'
 import './TeamPage.scss'
-
-
+//클로드는 항상 글로벌 시장을 목표로 하며, 성장을 해왔습니다.
+//앞으로 펼쳐질 클로드의 성장 스토리를 함께 그려나갈 훌륭한 동료를 기다립니다.
+// 저희는 같은 비전을 품은 사람들이 모였을 나오는 힘을 알고 있습니다.
+// 국내 많은 사람들이 카페와 편의점을 창업했고, 지금도 치킨집들이 망하지 않고 운영되고 있습니다. 
+// 개발자의 비전을 품은 IT회사 하나 정도 더 있어도 될 것 같다는 생각이 들었습니다. 
+// 그래서 개발자들의 놀이터인 클로드를 만들었습니다.
+// 대단한걸 하기 보다, 꾸준히 발전해서 마장동을 대표하는 IT회사가 되고 싶습니다.
+// 클로드는 누구나 성장할수 있다고 생각합니다. 자신의 맞는 습관, 환경이 존재한다면 말이죠.
+// 한계가 존재하면 다른방식으로 뛰어넘으면 됩니다.
+// 사용자들에게도 이런 가치를 전달하고 싶습니다.
 const TeamPage = () => (
 
   <div className="team">
@@ -12,40 +20,39 @@ const TeamPage = () => (
     <img className="team__image" src="resource/team3.png" alt="loading.." />
     <p className="team__text">Go Clrod. </p>
     <p className="team__text__sub">
-      클로드는 항상 글로벌 시장을 목표로 하며, 성장을 해왔습니다. <br />
-      앞으로 펼쳐질 클로드의 성장 스토리를 함께 그려나갈 훌륭한 동료를 기다립니다.
-      </p>
-
+    We are waiting for a great colleague.
+    </p>
+  
     <h3 className="team__title">Our Story</h3>
 
     <div className="team__contents">
       <p className="team__contents__text">
 
-      
-저희는 같은 비전을 품은 사람들이 모였을 나오는 힘을 알고 있습니다.
-국내 많은 사람들이 카페와 편의점을 창업했고, 지금도 치킨집들이 망하지 않고 운영되고 있습니다. 
-개발자의 비전을 품은 IT회사 하나 정도 더 있어도 될 것 같다는 생각이 들었습니다. 
-<img className="team__contents__image" src="resource/google.jpg" alt="loading.." />
-
- 
-<br/><br/>그래서 상상을 구현할수 있는 클로드를 만들었습니다.
-대단한걸 하기 보다, 꾸준히 발전해서 마장동을 대표하는 IT회사가 되고 싶습니다.
+      We know the power that comes from people who share the same vision. Many people have started cafes and convenience stores, and now the chicken houses are operated without closing down. We thought that there might be one more startup with a developer's vision.
+      <img className="team__contents__image" src="resource/google.jpg" alt="loading.." />
+      <br/><br/>
+So We created Clrod, a dev playground. We want to be an IT company that represents majang-dong by steadily growing rather than doing great things that change the world.
 <br/><br/>
-클로드는 누구나 성장할수 있다고 생각합니다. 자신의 맞는 습관, 환경이 존재한다면 말이죠.
-한계가 존재하면 다른방식으로 뛰어넘으면 됩니다.
-사용자들에게도 이런 가치를 전달하고 싶습니다.
+Clrod thinks that anyone can grow. If there is a proper habit and environment. If there is a limit, you can bypass it in a different way. We want to give this value to users.
+
       </p>
     </div>
-    <h3 className="team__title">Our Team</h3>
+    <h3 className="team__title">Members
+    </h3>
     <div className="team__photos">
-      <img className="team__photos__image" src="resource/Paul.jpg" alt="loading.." />
-      <img className="team__photos__image" src="resource/Kylie.png" alt="loading.." />
-
-    </div>
     <div>
-      
+      <img className="team__photos__image" src="resource/jw.png" alt="loading.." />
+      <p className="team__photos__text">backsoo(27)</p>
+      </div>
+      <div>
+      <img className="team__photos__image" src="resource/st.png" alt="loading.." />
+      <p className="team__photos__text">Otaku(27)</p>
+      </div>
+      <div>
+      <img className="team__photos__image" src="resource/cat.jpg" alt="loading.." />
+      <p className="team__photos__text" >Developer(3)</p>
+      </div>
     </div>
-
   </div>
 )
 
